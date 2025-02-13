6aed92927b7b3f044f3cdf857fe69d12470445cc
damienrigaudeau <91134839+damienrigaudeau@users.noreply.github.com>
2025-02-13 12:53:23 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ f = Fernet(key)
 
 @app.route('/encrypt/<string:valeur>')
 def encryptage(valeur):
-    valeur_bytes = valeur.encode()  # Conversion str -> bytesss
+    valeur_bytes = valeur.encode()  # Conversion str -> bytes
     token = f.encrypt(valeur_bytes)  # Encrypt la valeur
     return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
                                                                                                                                                      
